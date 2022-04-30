1ba079016aa14a26406bf1990a027784a3f81516
Leandro Chernij <lchernij@gmail.com>
2022-04-30 17:31:32 +0100

    Add admin profile

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        if (config('app.env') === 'local') {
+            \App\Models\User::factory(1)->isAdmin()->create([
+                'name' => 'Leandro Chernij',
+                'email' => 'leandro@local.com',
+                'password' => Hash::make('123123')
+            ]);
+        }
     }
 }
