afb4ab3466a29997c71143e9d347ec2f0c590d90
Leandro Chernij <lchernij@gmail.com>
2022-05-18 09:38:49 +0100

    14-create-crud-game-plataform
    - WIP Index tests

---
+++ tests/Feature/Admin/GamePlataform/IndexTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Feature\Admin\GamePlataform;
+
+use App\Models\GamePlataform;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IndexTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_game_plataform_screen_can_not_be_rendered_when_guess()
+    {
+        $response = $this->get('/hq/game-plataforms');
+
+        $response->assertStatus(302)
+            ->assertLocation('/hq/login');
+    }
+
+    public function test_game_plataform_screen_can_be_rendered_empty()
+    {
+        $this->setAdminUser();
+
+        $response = $this->get('/hq/game-plataforms');
+
+        $response->assertStatus(200)
+            ->assertSeeText("Game Plataform (0)")
+            ->assertSeeText("Add new")
+            ->assertSeeText("Empty list");
+    }
+
+    public function test_game_plataform_screen_can_be_rendered_with_one_result()
+    {
+        $this->setAdminUser();
+        $gamePlatform = GamePlataform::factory()->create();
+
+        $response = $this->get('/hq/game-plataforms');
+
+        $response->assertStatus(200)
+            ->assertSeeText("Game Plataform (1)")
+            ->assertSeeText("Add new")
+            ->assertSeeText($gamePlatform->name)
+            ->assertSeeText($gamePlatform->acronym)
+            ->assertSeeText($gamePlatform->company)
+            ->assertSeeText($gamePlatform->status)
+            ->assertSeeText(['More', 'Edit', 'Inactive']);
+    }
+
+    public function test_game_plataform_screen_can_be_rendered_pagination()
+    {
+        $this->setAdminUser();
+        GamePlataform::factory(16)->create();
+
+        $response = $this->get('/hq/game-plataforms');
+
+        $response->assertStatus(200)
+            ->assertSeeText("Game Plataform (16)");
+    }
+
+    private function setAdminUser(): void
+    {
+        $user = User::factory()->isAdmin()->create();
+
+        $this->actingAs($user);
+    }
+}
